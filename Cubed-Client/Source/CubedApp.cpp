574bf73bde90eea48bc9bccba49ae6cd930952a2
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-28 13:16:08 +0100

    Set title bar to false

--- Cubed-Client/Source/CubedApp.cpp
+++ Cubed-Client/Source/CubedApp.cpp
@@ -60,30 +60,11 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
 	spec.Name = "Walnut Example";
-	spec.CustomTitlebar = true;
+	spec.CustomTitlebar = false;
+	spec.UseDockspace = false;
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	std::shared_ptr<ExampleLayer> exampleLayer = std::make_shared<ExampleLayer>();
 	app->PushLayer(exampleLayer);
-	app->SetMenubarCallback([app, exampleLayer]()
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-			if (ImGui::MenuItem("Exit"))
-			{
-				app->Close();
-			}
-			ImGui::EndMenu();
-		}
-
-		if (ImGui::BeginMenu("Help"))
-		{
-			if (ImGui::MenuItem("About"))
-			{
-				exampleLayer->ShowAboutModal();
-			}
-			ImGui::EndMenu();
-		}
-	});
 	return app;
 }
